e326643d4335acedbdb3cce30990b2c526664125
b3-bogusz <b3.bogusz@gmail.com>
2021-09-27 22:01:34 +0200

    add components skeleton and routes

---
+++ src/components/GoogleAuth.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class GoogleAuth extends React.Component {
+    render() {
+        return (
+            <div>
+                Google Auth
+            </div>
+        )
+    };
+};
+
+export default GoogleAuth;
